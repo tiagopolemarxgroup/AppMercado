0176bb4ef3a64ecf72b4ff935c4f85cff0387eb7
Tiago Santos <tiago.polemarxgroup@gmail.com>
2022-12-09 16:36:03 -0300

    Projeto criado

---
+++ src/main/java/com/tiago/appmercado/AppmercadoApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.appmercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiago/appmercado/AppmercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.appmercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
